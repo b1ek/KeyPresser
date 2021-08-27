5bcec8d8da74e64e4b5b2e0342a54a9b02ad2b7e
blek <blek@DESKTOP-J1A13BI>
2021-08-27 19:28:04 +1000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿
+namespace KeyPresser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.y2label = new System.Windows.Forms.NumericUpDown();
+            this.y1label = new System.Windows.Forms.NumericUpDown();
+            this.x2label = new System.Windows.Forms.NumericUpDown();
+            this.x1label = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.smile = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.y2label)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y1label)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2label)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1label)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.smile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(-1, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KeyPresser";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.y2label);
+            this.groupBox1.Controls.Add(this.y1label);
+            this.groupBox1.Controls.Add(this.x2label);
+            this.groupBox1.Controls.Add(this.x1label);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(13, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(124, 188);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Область экрана";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 161);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 15);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Посмотреть";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Задать значение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // y2label
+            // 
+            this.y2label.Location = new System.Drawing.Point(39, 105);
+            this.y2label.Name = "y2label";
+            this.y2label.Size = new System.Drawing.Size(79, 23);
+            this.y2label.TabIndex = 1;
+            // 
+            // y1label
+            // 
+            this.y1label.Location = new System.Drawing.Point(39, 49);
+            this.y1label.Name = "y1label";
+            this.y1label.Size = new System.Drawing.Size(79, 23);
+            this.y1label.TabIndex = 1;
+            // 
+            // x2label
+            // 
+            this.x2label.Location = new System.Drawing.Point(39, 78);
+            this.x2label.Name = "x2label";
+            this.x2label.Size = new System.Drawing.Size(79, 23);
+            this.x2label.TabIndex = 1;
+            // 
+            // x1label
+            // 
+            this.x1label.Location = new System.Drawing.Point(39, 22);
+            this.x1label.Name = "x1label";
+            this.x1label.Size = new System.Drawing.Size(79, 23);
+            this.x1label.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(9, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Y2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(9, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Y1:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(9, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "X2:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(9, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "X1:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.linkLabel2);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Location = new System.Drawing.Point(144, 51);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(128, 188);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Действие";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(5, 53);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(72, 15);
+            this.linkLabel2.TabIndex = 6;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Что писать?";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(5, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "{F2}";
+            // 
+            // label6
+            // 
+            this.label6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label6.Location = new System.Drawing.Point(3, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 26);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Файл не выбран!";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(5, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Выбрать файл...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 103);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(108, 19);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Открыть файл:";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(6, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(119, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Нажать клавишу";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(7, 245);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Запуск";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // smile
+            // 
+            this.smile.Image = global::KeyPresser.Properties.Resources.utopia_smiley;
+            this.smile.Location = new System.Drawing.Point(138, 249);
+            this.smile.Name = "smile";
+            this.smile.Size = new System.Drawing.Size(17, 16);
+            this.smile.TabIndex = 4;
+            this.smile.TabStop = false;
+            this.smile.Click += new System.EventHandler(this.smile_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(162, 249);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 15);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Оно не работает!";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Location = new System.Drawing.Point(176, 8);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(56, 15);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Помощь";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 275);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.smile);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "KeyPresser";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.y2label)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.y1label)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x2label)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.x1label)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.smile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown y2label;
+        private System.Windows.Forms.NumericUpDown y1label;
+        private System.Windows.Forms.NumericUpDown x2label;
+        private System.Windows.Forms.NumericUpDown x1label;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox smile;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyPresser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            x1label.Maximum = Screen.PrimaryScreen.Bounds.Width;
+            y1label.Maximum = Screen.PrimaryScreen.Bounds.Height;
+            x2label.Maximum = Screen.PrimaryScreen.Bounds.Width;
+            y2label.Maximum = Screen.PrimaryScreen.Bounds.Height;
+
+            x1label.Value = RegistryConfig.x1;
+            y1label.Value = RegistryConfig.y1;
+            x2label.Value = RegistryConfig.x2;
+            y2label.Value = RegistryConfig.y2;
+
+            KeyPresser.areaChanged += new EventHandler(performActions);
+            this.FormClosed += (s, e) => { Environment.Exit(0); };
+            setSmileGray(true);
+        }
+        Keys keyToPress = Keys.F2;
+
+        private bool isCapture = false;
+        private void start_capture()
+        {
+            isCapture = true;
+            firstPos = true;
+        }
+
+        private bool firstPos = true;
+        private bool settingKeyToPress = false;
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData.HasFlag(Keys.Control) && keyData.HasFlag(Keys.H) && isCapture)
+            {
+                if (firstPos) // first pos
+                {
+                    x1label.Value = MousePosition.X;
+                    y1label.Value = MousePosition.Y;
+                    firstPos = false;
+                } else
+                {
+                    x2label.Value = MousePosition.X;
+                    y2label.Value = MousePosition.Y;
+                    isCapture = false;
+                    RegistryConfig.x1 = (int)x1label.Value;
+                    RegistryConfig.y1 = (int)y1label.Value;
+                    RegistryConfig.x2 = (int)x2label.Value;
+                    RegistryConfig.y2 = (int)y2label.Value;
+                }
+            }
+            if (settingKeyToPress)
+            {
+                keyToPress = keyData;
+                settingKeyToPress = false;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поставьте мышку в 1 угол\nи нажмите CTRL+H.\nПоставьте во второй угол и нажмите опять.\nНе забудьте держать фокус на главном окне.\n\nВы можете закрыть это окно.");
+            start_capture();
+        }
+
+        public void setSmileGray(bool status)
+        {
+            if (status)
+            {
+                Bitmap bitmap = (Bitmap) smile.Image;
+                int wid = bitmap.Width;
+                for (int x = 0; x < bitmap.Width; x++) {
+                    for (int y = 0; y < bitmap.Height; y++)
+                    {
+                        Color pix = bitmap.GetPixel(x, y);
+                        int mid = (pix.R + pix.G + pix.B) / 3;
+                        bitmap.SetPixel(x, y, Color.FromArgb(pix.A, mid, mid, mid));
+                    }
+                }
+                smile.Image = bitmap;
+                System.GC.Collect();
+            } else
+            {
+                smile.Image = global::KeyPresser.Properties.Resources.utopia_smiley;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = checkBox1.Checked;
+            if (!checkBox1.Checked && !checkBox2.Checked)
+            { button4.Enabled = false; }
+            else button4.Enabled = true;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            button3.Enabled = checkBox2.Checked;
+            if (!checkBox1.Checked && !checkBox2.Checked)
+            { button4.Enabled = false; }
+            else button4.Enabled = true;
+        }
+
+        private string pathtofile = "";
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog of = new OpenFileDialog();
+            DialogResult res = of.ShowDialog();
+            pathtofile = of.FileName;
+            MessageBox.Show($"Выбран файл: {pathtofile}");
+            string[] spl = of.FileName.Split("\\");
+            label6.Text = spl[spl.Length - 1];
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void smile_Click(object sender, EventArgs e)
+        {
+            setSmileGray(true);
+        }
+
+        public void initPresser()
+        {
+            KeyPresser.x1 = (int)x1label.Value;
+            KeyPresser.y1 = (int)y1label.Value;
+            KeyPresser.x2 = (int)x2label.Value;
+            KeyPresser.y2 = (int)y2label.Value;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            initPresser();
+            KeyPresser.getScreen().Save("a.png");
+            System.GC.Collect();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Viewer view = new Viewer();
+            view.Show();
+        }
+
+        private bool isWorking = false;
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked && pathtofile == "")
+            {
+                MessageBox.Show("Выберите файл!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!isWorking)
+            {
+                isWorking = true;
+                initPresser();
+                KeyPresser.run();
+                button4.Text = "Стоп!";
+                label7.Text = "Оно работает!";
+                setSmileGray(false);
+            } else
+            {
+                isWorking = false;
+                KeyPresser.stop();
+                button4.Text = "Запуск";
+                label7.Text = "Оно не работает!";
+                setSmileGray(true);
+            }
+        }
+
+        private void performActions(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) {
+                SendKeys.SendWait(textBox1.Text);
+                
+            }
+            if (checkBox2.Checked)
+            {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("cmd",
+                $"/c start {pathtofile}".Replace("&", "^&"))
+                { CreateNoWindow = true });
+            }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("cmd",
+                "/c start https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys?view=net-5.0#remarks".Replace("&", "^&"))
+                { CreateNoWindow = true });
+        }
+    }
+}
